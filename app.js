652005b298af2544d7f2161196b643d3df69b097
anrikawamo02-hash <anrikawamo02@gmail.com>
2026-02-17 20:46:41 +0900

    app.js を更新

--- app.js
+++ app.js
@@ -174,7 +174,7 @@
     } else {
 
   const roomNum = room.replace('rg', '');
-const basePath = `photos/${room}/${cat}/`;
+  const basePath = `photos/${room}/${cat}/`;
   const maxImages = 10;
   const slides = [];
 
