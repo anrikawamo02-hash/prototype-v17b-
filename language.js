1b61532e1682db2253ea4ce251434b50f597171d
anrikawamo02-hash <anrikawamo02@gmail.com>
2026-07-19 20:53:09 +0900

    Add files via upload

---
+++ language.js
@@ -0,0 +1,107 @@
+const translations = {
+  JP: {
+   roomSelect: "号室選択",
+   room12: "1・2号室",
+   room3: "3号室",
+   room4: "4号室",
+   room56: "5・6号室",
+   room78: "7・8号室",
+   room910_54: "9・10号室",
+   room910_32: "9・10号室",
+   room11: "11号室",
+  
+　　category: "カテゴリー",
+  
+   mainRoom: "メインルーム",
+　　kitchen: "キッチン",
+　　vanity: "洗面",
+　　bath: "バスルーム",
+　　toilet: "トイレ",
+　　closet: "クローゼット",
+　　entrance: "玄関",
+  },
+  EN: {
+  roomSelect: "ROOM SELECT",
+  room12: "Rooms 1-2",
+  room3: "Room 3",
+  room4: "Room 4",
+  room56: "Rooms 5-6",
+  room78: "Rooms 7-8",
+  room910_54: "Rooms 9-10",
+  room910_32: "Rooms 9-10",
+  room11: "Room 11",
+  
+  category: "Categories",
+  
+  mainRoom: "Main room",
+  kitchen: "Kitchen",
+  vanity: "Vanity",
+  bath: "Bath",
+  toilet: "Toilet",
+  closet: "Closet",
+  entrance: "Entrance"
+  },
+  ES: {
+    roomSelect: "SELECCIONAR HABITACIÓN",
+    room12: "Habitaciones 1-2",
+    room3: "Habitación 3",
+    room4: "Habitación 4",
+    room56: "Habitaciones 5-6",
+    room78: "Habitaciones 7-8",
+    room910_54: "Habitaciones 9-10",
+    room910_32: "Habitaciones 9-10",
+    room11: "Habitación 11",
+    
+    category: "Categorías",
+    
+    mainRoom: "Habitación principal",
+    kitchen: "Cocina",
+    vanity: "Tocador",
+    bath: "Baño",
+    toilet: "Inodoro",
+    closet: "Armario",
+    entrance: "Entrada"
+  },
+  KO: {
+    roomSelect: "객실 선택",
+    room12: "1·2호실",
+    room3: "3호실",
+    room4: "4호실",
+    room56: "5·6호실",
+    room78: "7·8호실",
+    room910_54: "9·10호실",
+    room910_32: "9·10호실",
+    room11: "11호실",
+    
+    category: "카테고리",
+    
+    mainRoom: "메인룸",
+    kitchen: "주방",
+    vanity: "세면대",
+    bath: "욕실",
+    toilet: "화장실",
+    closet: "옷장",
+    entrance: "현관"
+  },
+  ZH: {
+    roomSelect: "选择房间",
+    room12: "1・2号房",
+    room3: "3号房",
+    room4: "4号房",
+    room56: "5・6号房",
+    room78: "7・8号房",
+    room910_54: "9・10号房",
+    room910_32: "9・10号房",
+    room11: "11号房",
+    
+    category: "分类",
+    
+    mainRoom: "主卧室",
+    kitchen: "厨房",
+    vanity: "洗面区",
+    bath: "浴室",
+    toilet: "卫生间",
+    closet: "衣柜",
+    entrance: "玄关"
+  }
+};
